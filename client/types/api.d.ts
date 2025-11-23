ee63203fc59c663327bfcf611495ef76f030b232
Tymek <vonautymek@gmail.com>
2025-11-23 19:20:11 +0100

    client: generated types

--- client/types/api.d.ts
+++ client/types/api.d.ts
@@ -38,6 +38,23 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/user": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Get current user info */
+        get: operations["get_current_user"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -56,6 +73,20 @@ export interface components {
         };
         RegisterResponse: {
             success: boolean;
+        };
+        /**
+         * @example {
+         *       "error": "Unauthorized"
+         *     }
+         */
+        UnauthorizedError: {
+            error: string;
+        };
+        UserResponse: {
+            email: string;
+            /** Format: int32 */
+            id: number;
+            username: string;
         };
     };
     responses: never;
@@ -110,6 +141,35 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["RegisterResponse"];
+                };
+            };
+        };
+    };
+    get_current_user: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Success */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["UserResponse"];
+                };
+            };
+            /** @description Unauthorized */
+            401: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["UnauthorizedError"];
                 };
             };
         };
