7b68f90c849a39612048a5493db558c2c41b0cfb
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-11-23 20:03:47 +0100

    client: api check

--- client/components/user-profile.tsx
+++ client/components/user-profile.tsx
@@ -1,5 +1,6 @@
 import { apiBaseUrl } from "@/constants/apiBaseUrl";
 import { paths } from "@/types/api";
+import * as SecureStore from 'expo-secure-store';
 import createClient from "openapi-fetch";
 import { useEffect } from "react";
 import { View } from "react-native";
@@ -11,7 +12,22 @@ export default function UserProfile() {
     });
 
     useEffect(() => {
+        get()
     })
+
+
+
+    async function get() {
+        const token = await SecureStore.getItemAsync('token');
+        const { data, error, response } = await $api.GET("/api/user", {
+            headers: {
+                "Authorization": token,
+            }
+        });
+        console.log(data)
+        console.log(response)
+        console.log(error)
+    }
 
     return (
         <View>
