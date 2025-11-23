b35ccd9a05481b814504b9d7e094de31516feca3
Tymek <vonautymek@gmail.com>
2025-11-23 03:58:24 +0100

    client: added theming

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,12 +1,22 @@
 import '../global.css';
 
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import 'react-native-reanimated';
+import {
+    DarkTheme as NavDarkTheme,
+    DefaultTheme as NavLightTheme,
+    ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
+import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import Query from '@/lib/providers/query';
+import {
+    adaptNavigationTheme,
+    MD3DarkTheme,
+    MD3LightTheme,
+    PaperProvider,
+} from 'react-native-paper';
 
 export const unstable_settings = {
     anchor: '(tabs)',
@@ -14,18 +24,39 @@ export const unstable_settings = {
 
 export default function RootLayout() {
     const colorScheme = useColorScheme();
+    const { theme } = useMaterial3Theme();
+
+    const paperTheme =
+        colorScheme === 'dark'
+            ? { ...MD3DarkTheme, colors: theme.dark }
+            : { ...MD3LightTheme, colors: theme.light };
+
+    const { DarkTheme, LightTheme } = adaptNavigationTheme({
+        reactNavigationDark: NavDarkTheme,
+        reactNavigationLight: NavLightTheme,
+        materialDark: MD3DarkTheme,
+        materialLight: MD3LightTheme,
+    });
 
     return (
         <Query>
-            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-                <Stack>
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                    <Stack.Screen
-                        name="modal"
-                        options={{ presentation: 'modal', title: 'Modal' }}
-                    />
-                </Stack>
-                <StatusBar style="auto" />
+            <ThemeProvider
+                value={
+                    colorScheme === 'light'
+                        ? { ...LightTheme, fonts: NavLightTheme.fonts }
+                        : { ...DarkTheme, fonts: NavDarkTheme.fonts }
+                }
+            >
+                <PaperProvider theme={paperTheme}>
+                    <Stack>
+                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                        <Stack.Screen
+                            name="modal"
+                            options={{ presentation: 'modal', title: 'Modal' }}
+                        />
+                    </Stack>
+                    <StatusBar style="auto" />
+                </PaperProvider>
             </ThemeProvider>
         </Query>
     );
