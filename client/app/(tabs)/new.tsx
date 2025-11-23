05f471c1612bf408ce3f2ff5137a05e15254a3dc
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-11-23 21:35:13 +0100

    client: you need account to add notes

--- client/app/(tabs)/new.tsx
+++ client/app/(tabs)/new.tsx
@@ -1,32 +1,37 @@
 import { OCR } from "@dccarmo/react-native-ocr";
 import * as ImagePicker from "expo-image-picker";
 import * as SecureStore from 'expo-secure-store';
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from 'react-native';
 import { FAB, Text } from 'react-native-paper';
 export default function AddNewScreen() {
 
   const [result, setResult] = React.useState("No Result");
+  const [isLogged, setIsLogged] = useState<boolean>(false);
   const pickImage = async () => {
+    if(!isLogged){
+      alert("You need to log in in order to add note")
+      return;
+    }
+
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
       quality: 1,
     });
 
+    
     if (!result.canceled) {
       OCR.recognizeText(result.assets[0].uri).then(setResult);
       console.log(result);
     }
   };
 
-  const isLoged = checkLogin()
   async function checkLogin() {
     const res = await SecureStore.getItemAsync('token')
     if (res) {
       console.log("i am logged");
-      return true
+      setIsLogged(true)
     }
-
   }
 
   return (
@@ -35,7 +40,7 @@ export default function AddNewScreen() {
       style={styles.centerContainer}
     >
 
-    
+
       <FAB
         icon="plus"
         variant="primary"
